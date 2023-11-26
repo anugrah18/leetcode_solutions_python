94e8d4cef7e72881be8102178287581009c3acac
anugrah18 <anugrah18@gmail>
2023-11-25 20:56:27 -0800

    Added comments

--- Two pointers/125-validPalindrome.py
+++ Two pointers/125-validPalindrome.py
@@ -1,16 +1,23 @@
 class Solution(object):
     def isPalindrome(self, s):
+        # Initialize an empty string to store the alphanumeric characters in lowercase
         alphaNumericString = ""
 
+        # Iterate through each character in the input string
         for c in s:
             if (c.isalnum()):
+                # Append the lowercase alphanumeric character to the string
                 alphaNumericString = alphaNumericString + c.lower()
+
+        # Calculate the length of the alphanumeric string
         N = len(alphaNumericString)
 
+        # Iterate through the first half of the alphanumeric string
         for i in range(0, int(N / 2)):
             if (alphaNumericString[i] != alphaNumericString[N - i - 1]):
+                # If not equal, the string is not a palindrome
                 return False
-
+        # If the loop completes without returning, the string is a palindrome
         return True
 
 X = Solution()
