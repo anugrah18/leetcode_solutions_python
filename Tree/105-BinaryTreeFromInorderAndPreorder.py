b057f06472d59dcadd935b4324de8a4694e95239
anugrah18 <anugrah18@gmail>
2024-07-06 11:24:02 -0700

    Updated 105

--- Tree/105-BinaryTreeFromInorderAndPreorder.py
+++ Tree/105-BinaryTreeFromInorderAndPreorder.py
@@ -14,35 +14,34 @@ def Inorder(root):
 class Solution:
     def buildTree(self, preorder, inorder):
         def rec(inorder, preorder):
-            if not inorder or not preorder:
+            if not inorder or not preorder:  # Base case: if either list is empty, return
                 return
 
-            root = TreeNode(preorder.pop(0))
-            mid = inorder.index(root.val)
-            root.left = rec(inorder[:mid], preorder)
-            root.right = rec(inorder[mid + 1:], preorder)
+            root = TreeNode(preorder.pop(0))  # Create the root node from the first element of preorder
+            mid = inorder.index(root.val)  # Find the index of the root in inorder
+            root.left = rec(inorder[:mid], preorder)  # Recursively build the left subtree
+            root.right = rec(inorder[mid + 1:], preorder)  # Recursively build the right subtree
             return root
 
-        return rec(inorder, preorder)
+        return rec(inorder, preorder)  # Start the recursive building process
 
     def buildTree2(self, preorder, inorder):
-
-        mapper = {}
+        mapper = {}  # Dictionary to map values to their indices in inorder
         for i, v in enumerate(inorder):
             mapper[v] = i
 
         def helper(low, high):
-            if low > high:
+            if low > high:  # Base case: if the range is invalid, return
                 return
 
-            root = TreeNode(preorder.pop(0))
-            mid = mapper[root.val]
-            root.left = helper(low, mid - 1)
-            root.right = helper(mid + 1, high)
+            root = TreeNode(preorder.pop(0))  # Create the root node from the first element of preorder
+            mid = mapper[root.val]  # Get the index of the root from the mapper
+            root.left = helper(low, mid - 1)  # Recursively build the left subtree
+            root.right = helper(mid + 1, high)  # Recursively build the right subtree
 
             return root
 
-        return helper(0, len(inorder) - 1)
+        return helper(0, len(inorder) - 1)  # Start the recursive building process
 
 X= Solution()
 root = X.buildTree([3,9,20,15,7],[9,3,15,20,7])
