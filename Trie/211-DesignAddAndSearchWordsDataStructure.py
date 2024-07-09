9572d73edac5072aa56a88f34c0d4d214e87a68f
anugrah18 <anugrah18@gmail>
2024-07-08 22:20:15 -0700

    Update 211

--- Trie/211-DesignAddAndSearchWordsDataStructure.py
+++ Trie/211-DesignAddAndSearchWordsDataStructure.py
@@ -1,44 +1,38 @@
 class TrieNode:
     def __init__(self):
-        self.children = {}
-        self.word = False
-
+        self.children = {}  # Dictionary to store child nodes
+        self.word = False  # Boolean flag to indicate the end of a word
 
 class WordDictionary:
-
     def __init__(self):
-        self.root = TrieNode()
+        self.root = TrieNode()  # Initialize the WordDictionary with a root node
 
     def addWord(self, word: str) -> None:
-        curr = self.root
-
-        for c in word:
-            if c not in curr.children:
-                curr.children[c] = TrieNode()
-            curr = curr.children[c]
-
-        curr.word = True
+        curr = self.root  # Start from the root node
+        for c in word:  # Iterate over each character in the word
+            if c not in curr.children:  # If the character is not in the current node's children
+                curr.children[c] = TrieNode()  # Add a new TrieNode as a child
+            curr = curr.children[c]  # Move to the child node
+        curr.word = True  # Mark the end of the word
 
     def search(self, word: str) -> bool:
-
         def dfs(j, root):
-            curr = root
-            for i in range(j, len(word)):
+            curr = root  # Start from the given node
+            for i in range(j, len(word)):  # Iterate over each character in the word starting from index j
                 c = word[i]
-                if c == ".":
-                    for child in curr.children.values():
-                        if dfs(i + 1, child):
+                if c == ".":  # If the character is a dot, it can match any character
+                    for child in curr.children.values():  # Check all possible children nodes
+                        if dfs(i + 1, child):  # Recursively search for the rest of the word
                             return True
-                    return False
-
+                    return False  # If no match is found, return False
                 else:
-                    if c not in curr.children:
-                        return False
-                    curr = curr.children[c]
+                    if c not in curr.children:  # If the character is not found in the current node's children
+                        return False  # The word does not exist in the trie
+                    curr = curr.children[c]  # Move to the child node
+            return curr.word  # Return True if the current node marks the end of the word
 
-            return curr.word
+        return dfs(0, self.root)  # Start the DFS traversal from the root and the first character of the word
 
-        return dfs(0, self.root)
 
 wordDictionary = WordDictionary()
 print(wordDictionary.addWord("bad"))
