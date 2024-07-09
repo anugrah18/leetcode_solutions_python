214b080ec5132f9a61af82e7924aac1f291dbe1a
anugrah18 <anugrah18@gmail>
2024-07-08 22:27:49 -0700

    Updated 212

--- Trie/212-wordSearchII.py
+++ Trie/212-wordSearchII.py
@@ -1,51 +1,55 @@
 class TrieNode:
     def __init__(self):
-        self.children = {}
-        self.isWord = False
+        self.children = {}  # Dictionary to store child nodes
+        self.isWord = False  # Boolean flag to indicate the end of a word
 
     def addWord(self, word):
-        curr = self
-        for c in word:
-            if c not in curr.children:
-                curr.children[c] = TrieNode()
-            curr = curr.children[c]
-        curr.isWord = True
-
+        curr = self  # Start from the current TrieNode
+        for c in word:  # Iterate over each character in the word
+            if c not in curr.children:  # If the character is not in the current node's children
+                curr.children[c] = TrieNode()  # Add a new TrieNode as a child
+            curr = curr.children[c]  # Move to the child node
+        curr.isWord = True  # Mark the end of the word
 
 class Solution:
-    def findWords(self, board,  words):
-        root = TrieNode()
-        for w in words:
+    def findWords(self, board, words):
+        root = TrieNode()  # Initialize the trie with a root node
+        for w in words:  # Add all words to the trie
             root.addWord(w)
-        ROWS, COLS = len(board), len(board[0])
+        ROWS, COLS = len(board), len(board[0])  # Number of rows and columns in the board
 
-        res, visit = set(), set()
+        res, visit = set(), set()  # Initialize result set and visited set
 
         def dfs(r, c, node, word):
+            # Base case: if out of bounds or already visited or character not in current node's children
             if (r < 0 or c < 0 or
                     r == ROWS or
                     c == COLS or
                     (r, c) in visit or
                     board[r][c] not in node.children):
                 return
-            visit.add((r, c))
-            node = node.children[board[r][c]]
-            word += board[r][c]
-            if node.isWord:
-                res.add(word)
+            visit.add((r, c))  # Add current cell to visited set
+            node = node.children[board[r][c]]  # Move to the child node
+            word += board[r][c]  # Append current character to the word
+            if node.isWord:  # If the current node marks the end of a word
+                res.add(word)  # Add the word to the result set
+            # Recursively search in all four directions
             dfs(r + 1, c, node, word)
             dfs(r - 1, c, node, word)
             dfs(r, c + 1, node, word)
             dfs(r, c - 1, node, word)
+            visit.remove((r, c))  # Remove current cell from visited set
 
-            visit.remove((r, c))
-
-        for r in range(ROWS):
+        for r in range(ROWS):  # Iterate over each cell in the board
             for c in range(COLS):
-                dfs(r, c, root, "")
+                dfs(r, c, root, "")  # Start DFS from each cell
 
-        return list(res)
+        return list(res)  # Return the result as a list
+
 
 
 X= Solution()
 print(X.findWords([["o","a","a","n"],["e","t","a","e"],["i","h","k","r"],["i","f","l","v"]],["oath","pea","eat","rain"]))
+
+# Time Complexity : O(N*M*L) , Where L is length of word, N = num of rows, M = num of cols
+# Space Complexity: O(N*M)
