2785998278358e3f03eae12c87d051ce8f254aa8
anugrah18 <anugrah18@gmail>
2024-07-12 07:36:31 -0700

    Updated 133

--- Graphs/133-cloneGraph.py
+++ Graphs/133-cloneGraph.py
@@ -1,31 +1,41 @@
 class Node:
-    def __init__(self, val = 0, neighbors = None):
+    def __init__(self, val=0, neighbors=None):
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
 
 class Solution:
-    visited = {}
-    def cloneGraph(self,node):
+    visited = {}  # A class-level dictionary to keep track of visited nodes
+
+    def cloneGraph(self, node):
+        # If the input node is None, return None
         if not node:
             return
+
+        # If the node is already visited, return its clone from the visited dictionary
         if node in self.visited:
             return self.visited[node]
 
-        clone_node = Node(node.val,[])
-        self.visited[node]=clone_node
+        # Create a clone of the current node
+        clone_node = Node(node.val, [])
+        # Add the clone to the visited dictionary
+        self.visited[node] = clone_node
 
+        # Iterate through the neighbors of the node and recursively clone them
         if node.neighbors:
             for neighbor in node.neighbors:
                 clone_node.neighbors.append(self.cloneGraph(neighbor))
 
         return clone_node
 
-    def dfs(self,node,visit=[]):
+    def dfs(self, node, visit=[]):
+        # Append the current node's value to the visit list
         visit.append(node.val)
+        # Print the current node's value
         print(node.val)
+        # Recursively perform DFS on unvisited neighbors
         for neighbor in node.neighbors:
             if neighbor.val not in visit:
-                self.dfs(neighbor,visit)
+                self.dfs(neighbor, visit)
 
 X = Solution()
 node1 = Node(1,[])
