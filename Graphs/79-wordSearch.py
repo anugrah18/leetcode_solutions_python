ce1c344e3cb8f5899cc81df9b1fae0cb936d52c8
anugrah18 <anugrah18@gmail>
2024-07-12 07:13:14 -0700

    Updated 79

--- Graphs/79-wordSearch.py
+++ Graphs/79-wordSearch.py
@@ -1,31 +1,43 @@
 class Solution:
-    def exist(self,board,word):
-        for i in range(0,len(board)):
-            for j in range(0,len(board[i])):
-                if (board[i][j]==word[0] and self.dfs(board,i,j,0,word)):
+    def exist(self, board, word):
+        # Iterate through each cell in the board
+        for i in range(0, len(board)):
+            for j in range(0, len(board[i])):
+                # Check if the current cell matches the first letter of the word
+                # and start a DFS search from there
+                if board[i][j] == word[0] and self.dfs(board, i, j, 0, word):
                     return True
 
+        # If no matching path is found, return False
         return False
 
-
-    def dfs(self,board,i,j,count,word):
-        if count==len(word):
+    def dfs(self, board, i, j, count, word):
+        # If all characters are matched, return True
+        if count == len(word):
             return True
 
-        if(i<0 or i>len(board)-1 or j<0 or j>len(board[i])-1 or board[i][j]!=word[count]):
+        # If the current position is out of bounds or does not match the character, return False
+        if i < 0 or i >= len(board) or j < 0 or j >= len(board[i]) or board[i][j] != word[count]:
             return False
 
+        # Temporarily mark the board cell as visited
         temp = board[i][j]
-        board[i][j]="#"
-        found = self.dfs(board,i+1,j,count+1,word) or self.dfs(board,i-1,j,count+1,word) or \
-        self.dfs(board, i, j+1, count + 1, word) or self.dfs(board,i,j-1,count+1,word)
+        board[i][j] = "#"
 
+        # Perform DFS in all four possible directions
+        found = (self.dfs(board, i + 1, j, count + 1, word) or
+                 self.dfs(board, i - 1, j, count + 1, word) or
+                 self.dfs(board, i, j + 1, count + 1, word) or
+                 self.dfs(board, i, j - 1, count + 1, word))
+
+        # Restore the cell's original value
         board[i][j] = temp
 
         return found
 
+
 X = Solution()
 print(X.exist([["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]],"ABCCED"))
 
-# Time Complexity : O(MN) , MN = lenght of grid
-# Space Complexity : O(L) , L = length of the word to be matched
+# Time Complexity : O(M*N*4^L) , M = num of rows , N = num of columns , L = length of word
+# Space Complexity : O(L)
