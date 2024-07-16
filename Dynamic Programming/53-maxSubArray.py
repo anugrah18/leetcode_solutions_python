8d16deab968fc9fe8b63578a074e482c382de06e
anugrah18 <anugrah18@gmail>
2024-07-15 21:34:59 -0700

    Updated 53

--- Dynamic Programming/53-maxSubArray.py
+++ Dynamic Programming/53-maxSubArray.py
@@ -1,21 +1,34 @@
 class Solution(object):
     def maxSubArray(self, nums):
-
+        # Initialize maxSum to the first element of the array
         maxSum = nums[0]
-        currentSum  = nums[0]
+        # Initialize currentSum to the first element of the array
+        currentSum = nums[0]
+        # Start the loop from the second element
         i = 1
-        while(i<len(nums)):
-            if(nums[i]>currentSum+nums[i]):
+
+        # Iterate through the array
+        while i < len(nums):
+            # If adding the current element to currentSum is less than the current element itself,
+            # start a new subarray from the current element
+            if nums[i] > currentSum + nums[i]:
                 currentSum = nums[i]
             else:
+                # Otherwise, extend the current subarray
                 currentSum = currentSum + nums[i]
-            maxSum = currentSum if currentSum > maxSum else maxSum
-            i = i+1
+
+            # Update maxSum to be the maximum of maxSum and currentSum
+            maxSum = max(maxSum, currentSum)
+            # Move to the next element
+            i = i + 1
+
+        # Return the maximum sum of any subarray found
         return maxSum
 
 
+
 X = Solution()
-ans = X.maxSubArray([-2,1,-3,4,-1,2,1,-5,4])
+ans = X.maxSubArray([5,4,-1,7,8])
 
 print(ans)
 
