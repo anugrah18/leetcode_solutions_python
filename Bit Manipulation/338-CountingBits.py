e7f339bdd4bcec91bb2077f25116ec92d85740e3
anugrah18 <anugrah18@gmail>
2024-07-16 17:51:08 -0700

    Updated 338

--- Bit Manipulation/338-CountingBits.py
+++ Bit Manipulation/338-CountingBits.py
@@ -1,13 +1,23 @@
 class Solution:
     def countBits(self, n):
+        # Initialize an array to store the number of 1's for each number from 0 to n
         dp = [0] * (n + 1)
         offset = 1
+
+        # Loop through each number from 1 to n
         for i in range(1, n + 1):
+            # Update the offset when i is a power of 2
             if offset * 2 == i:
                 offset = i
+            # Calculate the number of 1's for the current number i
             dp[i] = 1 + dp[i - offset]
 
+        # Return the array containing the number of 1's for each number from 0 to n
         return dp
+
 
 X= Solution()
 print(X.countBits(2))
+
+# Time Complexity: O(N)
+# Space Complexity: O(N)
