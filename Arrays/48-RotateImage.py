c05e7896a3bcdd6112af0d634821b7a33f0d193f
anugrah18 <anugrah18@gmail>
2024-07-16 17:39:13 -0700

    Updated 48

--- Arrays/48-RotateImage.py
+++ Arrays/48-RotateImage.py
@@ -3,13 +3,17 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        N = len(matrix)
+        N = len(matrix)  # Get the size of the matrix
+
+        # Transpose the matrix by swapping elements at symmetric positions
         for i in range(N):
             for j in range(i + 1, N):
                 matrix[j][i], matrix[i][j] = matrix[i][j], matrix[j][i]
 
+        # Reverse each row to get the rotated matrix
         for i in range(N):
-            matrix[i] = (matrix[i][::-1])
+            matrix[i] = matrix[i][::-1]
+
 
 matrix = [[1,2,3],[4,5,6],[7,8,9]]
 X = Solution()
