fee1e785dc2c598d0cd1a994ff1c36954ea64ff2
anugrah18 <anugrah18@gmail>
2024-07-16 17:57:10 -0700

    Updated 268

--- Bit Manipulation/268-missingNumber.py
+++ Bit Manipulation/268-missingNumber.py
@@ -1,10 +1,17 @@
 class Solution:
     def missingNumber(self, nums) -> int:
+        # Initialize res to the length of nums
         res = len(nums)
 
+        # Calculate the expected sum of indices and actual sum of nums
         for i in range(len(nums)):
             res += (i - nums[i])
+
         return res
+
 
 X = Solution()
 print(X.missingNumber([3,0,1]))
+
+# Time Complexity : O(N)
+# Space Complexity : O(1)
