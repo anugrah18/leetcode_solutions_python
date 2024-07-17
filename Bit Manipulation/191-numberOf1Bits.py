4cd41d847cba6fd3c3a198df2f5e09d2fa8b9aa7
anugrah18 <anugrah18@gmail>
2024-07-16 17:48:21 -0700

    Updated 191

--- Bit Manipulation/191-numberOf1Bits.py
+++ Bit Manipulation/191-numberOf1Bits.py
@@ -1,10 +1,15 @@
 class Solution:
     def hammingWeight(self, n):
         res = 0
+        # Loop until all bits are processed
         while n:
-            res+= n % 2
+            # Add the least significant bit (0 or 1) to the result
+            res += n % 2
+            # Right shift the bits of n to process the next bit
             n = n >> 1
         return res
-
 X = Solution()
 print(X.hammingWeight(11))
+
+# Time Complexity : O(N) , N = integer whose bits are to be counted
+# Space Complexity : O(1)
