98f82a32a779512498d9dfba1478bb1b1075ff44
anugrah18 <anugrah18@gmail>
2024-07-16 17:28:59 -0700

    Updated 435 and 56

--- Arrays/435-nonOverlappingIntervals.py
+++ Arrays/435-nonOverlappingIntervals.py
@@ -1,17 +1,27 @@
 class Solution:
     def eraseOverlapIntervals(self, intervals):
+        # Sort the intervals based on their starting times
         intervals = sorted(intervals, key=lambda x: x[0])
 
+        # Initialize the result counter for the number of intervals to remove
         res = 0
+        # Initialize prevEnd to the end of the first interval
         prevEnd = intervals[0][1]
+
+        # Iterate through the intervals starting from the second interval
         for start, end in intervals[1:]:
+            # If there is no overlap, update prevEnd to the current end
             if start >= prevEnd:
                 prevEnd = end
             else:
+                # If there is an overlap, increment the result counter
                 res += 1
+                # Update prevEnd to the minimum end time to maximize the non-overlapping space
                 prevEnd = min(end, prevEnd)
 
+        # Return the number of intervals to remove to make the rest non-overlapping
         return res
+
 
 X = Solution()
 print(X.eraseOverlapIntervals([[1,2],[2,3],[3,4],[1,3]]))
--- Arrays/56-mergedIntervals.py
+++ Arrays/56-mergedIntervals.py
@@ -1,15 +1,24 @@
-
 class Solution(object):
     def merge(self, intervals):
+        # Sort the intervals based on the starting times
         intervals.sort(key=lambda x: x[0])
+
+        # Initialize an empty list to store merged intervals
         merged = []
+
+        # Iterate through each interval
         for interval in intervals:
-            if (len(merged) == 0 or merged[-1][1] < interval[0]):
+            # If merged is empty or the last interval in merged does not overlap with the current interval
+            if len(merged) == 0 or merged[-1][1] < interval[0]:
+                # Append the current interval to merged
                 merged.append(interval)
             else:
+                # There is an overlap, so merge the current interval with the last interval in merged
                 merged[-1][1] = max(merged[-1][1], interval[1])
 
+        # Return the list of merged intervals
         return merged
+
 
 X = Solution()
 print(X.merge([[1,5],[2,9],[13,15]]))
