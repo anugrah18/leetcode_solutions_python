4ce9e0628d931845daa37024f6bceabaed6639ef
anugrah18 <anugrah18@gmail>
2024-07-16 17:34:25 -0700

    Updated 253

--- Greedy/253-meetingRoomsII.py
+++ Greedy/253-meetingRoomsII.py
@@ -6,23 +6,30 @@ class Solution:
         e = 0
         s = 0
 
+        # Separate start and end times from intervals
         for i in range(len(intervals)):
             startTimings.append(intervals[i][0])
             endTimings.append(intervals[i][1])
 
+        # Sort start and end times
         startTimings.sort()
         endTimings.sort()
 
+        # Use two pointers to compare start and end times
         while s < len(intervals):
             if startTimings[s] < endTimings[e]:
+                # If a meeting starts before the earliest ending meeting ends, we need a new room
                 s += 1
                 rooms += 1
             else:
+                # If a meeting starts after the earliest ending meeting ends, we can reuse a room
                 e += 1
                 rooms -= 1
+            # Update the maximum number of rooms needed
             res = max(rooms, res)
 
         return res
+
 
 
 X = Solution()
