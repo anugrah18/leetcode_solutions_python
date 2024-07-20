b308ffc053ef06a081c3c2f6b4395eb5270a38e8
anugrah18 <anugrah18@gmail>
2024-07-20 11:45:38 -0700

    Updated 122

--- Arrays/122-bestTimeToBuyStock-II.py
+++ Arrays/122-bestTimeToBuyStock-II.py
@@ -1,10 +1,16 @@
 class Solution(object):
     def maxProfit(self, prices):
+        # Initialize the maximum profit to 0
         maxProfit = 0
-        for i in range(1, len(prices)):
-            if (prices[i] > prices[i - 1]):
-                maxProfit = maxProfit + prices[i] - prices[i - 1]
 
+        # Iterate through the list of prices from the second day
+        for i in range(1, len(prices)):
+            # If the current price is greater than the previous day's price
+            if prices[i] > prices[i - 1]:
+                # Add the difference to the maximum profit
+                maxProfit += prices[i] - prices[i - 1]
+
+        # Return the total maximum profit
         return maxProfit
 
 
