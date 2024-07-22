3fd98fb6b4bae9ae28d6f26e84974dd6c1c613fa
anugrah18 <anugrah18@gmail>
2024-07-22 07:14:03 -0700

    Updated 442

--- Arrays/442-findAllDuplicatesInArray.py
+++ Arrays/442-findAllDuplicatesInArray.py
@@ -1,13 +1,19 @@
 class Solution:
     def findDuplicates(self, nums):
+        # Initialize a set to store the duplicate numbers
         ans = set()
 
+        # Iterate through each number in the list
         for n in nums:
-            if nums[abs(n)-1]<0:
+            # If the number at the index abs(n)-1 is negative, it means the number n is a duplicate
+            if nums[abs(n) - 1] < 0:
+                # Add the absolute value of n to the set of duplicates
                 ans.add(abs(n))
             else:
-                nums[abs(n)-1]*=-1
+                # Otherwise, negate the number at the index abs(n)-1 to mark it as visited
+                nums[abs(n) - 1] *= -1
 
+        # Convert the set of duplicates to a list and return it
         return list(ans)
 
 X = Solution()
