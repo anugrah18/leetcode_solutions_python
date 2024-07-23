6612398c626e00a8d6cec4d6470b31a1d2801795
anugrah18 <anugrah18@gmail>
2024-07-22 20:41:53 -0700

    Updated 1465

--- Arrays/1465-maxAreaOfCakeAfterCuts.py
+++ Arrays/1465-maxAreaOfCakeAfterCuts.py
@@ -1,23 +1,29 @@
 class Solution:
-    def maxArea(self,h,w,horizontalCuts,verticalCuts):
+    def maxArea(self, h: int, w: int, horizontalCuts: list, verticalCuts: list) -> int:
+        # Add the edges of the cake to the cuts list
         horizontalCuts.append(0)
         horizontalCuts.append(h)
         verticalCuts.append(0)
         verticalCuts.append(w)
 
+        # Sort the cuts to find the maximum differences
         horizontalCuts.sort()
         verticalCuts.sort()
 
+        # Initialize variables to store the maximum differences
         h_diff = -1
-        w_diff =-1
+        w_diff = -1
 
-        for i in range(1,len(horizontalCuts)):
-            h_diff = max(h_diff,horizontalCuts[i]-horizontalCuts[i-1])
+        # Find the maximum difference between consecutive horizontal cuts
+        for i in range(1, len(horizontalCuts)):
+            h_diff = max(h_diff, horizontalCuts[i] - horizontalCuts[i-1])
 
-        for i in range(1,len(verticalCuts)):
-            w_diff = max(w_diff,verticalCuts[i]-verticalCuts[i-1])
+        # Find the maximum difference between consecutive vertical cuts
+        for i in range(1, len(verticalCuts)):
+            w_diff = max(w_diff, verticalCuts[i] - verticalCuts[i-1])
 
-        return (h_diff*w_diff)%(1000000007)
+        # Calculate the maximum area and return it modulo 10^9 + 7
+        return (h_diff * w_diff) % (1000000007)
 
 X = Solution()
 print(X.maxArea(5,4,[1,2,4],[1,3]))
