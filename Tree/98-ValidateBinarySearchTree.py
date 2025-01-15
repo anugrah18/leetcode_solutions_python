0f81f2a84a20d51dbe445f12b17eaa03cb210064
anugrah18 <anugrah18@gmail>
2025-01-15 08:29:57 -0600

    Updated Tree

--- Tree/98-ValidateBinarySearchTree.py
+++ Tree/98-ValidateBinarySearchTree.py
@@ -5,7 +5,8 @@ class TreeNode:
         self.right = right
 
 class Solution:
-    def isValidBST(self, root) -> bool:
+    # By Recursion
+    def isValidBST_I(self, root) -> bool:
 
         def inorder(root):
             if not root:
@@ -28,13 +29,40 @@ class Solution:
         self.prev = -float('inf')  # Initialize previous node value to negative infinity
         return inorder(root)  # Start the inorder traversal from the root
 
+    # By Iteration
+    def isValidBST_II(self, root) -> bool:
+        # Initialize an empty list to store the in-order traversal result
+        self.ans = []
+
+        def inorder(root):
+            if not root:
+                return
+            # Recursively traverse the left subtree
+            inorder(root.left)
+            # Append the value of the current node to the result list
+            self.ans.append(root.val)
+            # Recursively traverse the right subtree
+            inorder(root.right)
+
+        # Perform an in-order traversal of the tree
+        inorder(root)
+        # Check if the in-order traversal result is sorted in strictly increasing order
+        for i in range(1, len(self.ans)):
+            # If a previous value is greater than or equal to the current value,
+            # the tree is not a valid Binary Search Tree (BST)
+            if self.ans[i - 1] >= self.ans[i]:
+                return False
+        # If no violations are found, the tree is a valid BST
+        return True
+
 
 root = TreeNode(2)
 root.left = TreeNode(1)
 root.right = TreeNode(3)
 
 X = Solution()
-print(X.isValidBST(root))
+print(X.isValidBST_I(root))
+print(X.isValidBST_II(root))
 
 # Time Complexity = O(N)
 # Space Complexity = O(N)
