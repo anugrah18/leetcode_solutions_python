312debc84825f5d4bf4120691abad9f42b0104be
anugrah18 <anugrah18@gmail>
2025-05-07 23:58:14 -0500

    Updated Graph

--- Graphs/210-courseScheduleII.py
+++ Graphs/210-courseScheduleII.py
@@ -1,38 +1,30 @@
-class Solution(object):
-    def findOrder(self, numCourses, prerequisites):
+class Solution:
+    def findOrder(self, numCourses ,prerequisites):
         queue = []
         graph = {}
         indegree = [0] * numCourses
-        count = 0
-        res = []
+        ans = []
 
         for i in range(numCourses):
             graph[i] = []
 
-        for course in prerequisites:
-            graph[course[1]].append(course[0])
-            indegree[course[0]] += 1
+        for courses in prerequisites:
+            graph[courses[1]].append(courses[0])
+            indegree[courses[0]]+=1
 
-        for i in range(len(indegree)):
-            if (indegree[i] == 0):
+        for i in range(numCourses):
+            if indegree[i] == 0:
                 queue.append(i)
-                res.append(i)
 
-        while (queue):
+        while queue:
             curr = queue.pop(0)
-            neighbors = graph[curr]
-            count += 1
-
-            for neighbor in neighbors:
-                indegree[neighbor] -= 1
-                if (indegree[neighbor] == 0):
+            ans.append(curr)
+            for neighbor in graph[curr]:
+                indegree[neighbor]-=1
+                if indegree[neighbor] == 0:
                     queue.append(neighbor)
-                    res.append(neighbor)
 
-        if (count == numCourses):
-            return res
-        else:
-            return []
+        return ans if len(ans)==numCourses else []
 
 
 X = Solution()
